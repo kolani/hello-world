38cb0623d6dc9e3d7c186f720a5df07d7f260549
kolani <kolani83@hotmail.com>
2016-05-08 00:10:12 -0400

    autobuild test

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -2,5 +2,6 @@ class JavaHelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!"); // Display the string.
 	System.out.println("Hello World!");
+	System.out.println("Autobuild :)");
     }
 }
